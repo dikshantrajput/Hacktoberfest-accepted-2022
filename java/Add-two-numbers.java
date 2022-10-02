395b1c3fd1118f9e88115d35f408f61dc7642edd
Kashish Lakhara <104296574+KashishLakhara04@users.noreply.github.com>
2022-10-01 21:22:30 -0700

    Created Add-minimum-no-of-rungs.java
    Created Add-two-numbers.java
    
    Create Add-spaces-to-string.java
    
    Created Add-minimum-no-of-rungs.java

---
+++ java/Add-two-numbers.java
@@ -0,0 +1,26 @@
+class Solution {
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode root = new ListNode(-1);
+    ListNode curr = root;
+    int carry = 0;
+    while (l1 != null || l2 != null || carry != 0) {
+      int currSum = carry;
+      if (l1 != null && l2 != null) {
+        currSum += l1.val + l2.val;
+        l1 = l1.next;
+        l2 = l2.next;
+      } else if (l1 != null && l2 == null) {
+        currSum += l1.val;
+        l1 = l1.next;
+      } else if (l1 == null && l2 != null) {
+        currSum += l2.val;
+        l2 = l2.next;
+      }
+      carry = currSum > 9 ? 1 : 0;
+      currSum %= 10;
+      curr.next = new ListNode(currSum);
+      curr = curr.next;
+    }
+    return root.next;
+  }
+}

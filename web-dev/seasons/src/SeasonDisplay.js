94a337dfbfd6138d809648a484c4fe8890bb1380
Siddharth Bhat <siddharthbhat44@gmail.com>
2022-10-02 20:19:20 +0530

    Siddharth Bhat contribution

---
+++ web-dev/seasons/src/SeasonDisplay.js
@@ -0,0 +1,34 @@
+import './SeasonDisplay.css';
+import React from "react";
+const seasonConfig={
+    summer: {
+        text:"Bhai bohot garmi hai",
+        iconName: "sun"
+    },
+    winter: {
+        text: "Bhai bohot thand hai",
+        iconName: "snowflake"
+    }
+
+};
+const getSeason = (lat,month)=>{
+    if(month>2 && month<9)
+    {
+        return lat > 0 ? 'summer': 'winter';
+    }else
+    {
+        return lat > 0 ? 'winter': 'summer';
+    }
+};
+const SeasonDisplay =(props) => {
+    const season  = getSeason(props.lat, new Date().getMonth());
+    const {text,iconName} =seasonConfig[season];
+    return(
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
+           <h1>{text}</h1> 
+           <i className={`icon-right massive ${iconName} icon`}/>
+        </div>
+    );
+};
+export default SeasonDisplay;

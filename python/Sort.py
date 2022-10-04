bf3a9613159b39dcac8d46562db2bd2ba403b25e
IamSofiyan <72224257+IamSofiyan@users.noreply.github.com>
2022-10-04 16:31:09 +0530

    Create Sort.py

---
+++ python/Sort.py
@@ -0,0 +1,6 @@
+def sort(a, b): 
+  if(sorted(a)== sorted(b)): print("The strings are anagrams.") 
+    else: print("The strings are not sorted.") 
+      a = "debit card"
+      b = "bad credit"
+      sort(a,b)

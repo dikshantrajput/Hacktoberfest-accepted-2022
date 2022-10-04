7c6168af2fb722eec0b7ebb436466c00fa7f7e54
bye818 <114975134+bye818@users.noreply.github.com>
2022-10-03 22:42:09 -0700

    Create greetermachine.js

---
+++ greetermachine.js
@@ -0,0 +1,12 @@
+var name = "Bye"
+var number = 1 
+var number2 = 2
+
+var answer = number+ name 
+console.log(answer) 
+
+var note = name+ "hello!"
+
+var note2 = '${name} hello'
+
+console.log(note2)

0f087bdcc0f1729b9e96dc4b07c39f63a4f9b635
Kush-munot <munot.kgm@gmail.com>
2022-10-04 18:44:46 +0530

    Added Odd Even Game Code in Codeforces Solution

---
+++ cpp/CodeForces Solutions/odd even game.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << "Mahmoud" << endl;
+    }
+    else
+    {
+        cout << "Ehab" << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    solve();
+}

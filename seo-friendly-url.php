a6081afecd5bf4305bf5fd6956d5fbe752a762a1
Aditya Naidu <32679846+adi-rebellion@users.noreply.github.com>
2022-10-06 00:52:48 +0530

    Create seo-friendly-url.php
    A function in php which will take in a string an convert it into an seo friendly slug.

---
+++ seo-friendly-url.php
@@ -0,0 +1,9 @@
+ public function seo_friendly_url($string){
+        $string = str_replace(array('[\', \']'), '-', $string);
+        $string = preg_replace('/\[.*\]/U', '-', $string);
+        $string = preg_replace('/&(amp;)?#?[a-z0-9]+;/i', '-', $string);
+        $string = htmlentities($string, ENT_COMPAT, 'utf-8');
+        $string = preg_replace('/&([a-z])(acute|uml|circ|grave|ring|cedil|slash|tilde|caron|lig|quot|rsquo);/i', '\\1', $string );
+        $string = preg_replace(array('/[^a-z0-9]/i', '/[-]+/') , '-', $string);
+        return strtolower(trim($string, '-'));
+    }

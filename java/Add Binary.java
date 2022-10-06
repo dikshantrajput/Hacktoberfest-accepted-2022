95d3c8e4f46169a6bcf7435a6aa250cd9df0b0ce
Savijain <81694833+Savijain@users.noreply.github.com>
2022-10-06 12:31:04 +0530

    Update Add Binary.java

--- java/Add Binary.java
+++ java/Add Binary.java
@@ -1,4 +1,9 @@
 //Given two binary strings a and b, return their sum as a binary string.
+/*Example
+Input: a = "1010", b = "1011"
+Output: "10101"
+*/
+
 
 class Solution {
     public String addBinary(String a, String b) {

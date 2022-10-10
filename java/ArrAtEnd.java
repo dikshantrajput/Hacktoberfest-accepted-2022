1ee2f4af5cd10160f970604ee542d925d01c3a65
aditya007 <56697436+Aditya007-cyber@users.noreply.github.com>
2022-10-10 23:31:10 +0530

    Update ArrAtEnd.java

--- java/ArrAtEnd.java
+++ java/ArrAtEnd.java
@@ -3,12 +3,14 @@ public class ArrAtEnd{
 	public static void main(String args[])
 	{
 		int arr[]= {2,4,6};
+		ArrayList<Integer> list = new ArrayList<>();
 		int element =8;
-		int newArr[]=Arrays.copyOf(arr, arr.length+1);
-		newArr[arr.length]=element;
+		list.add(element);
+// 		int newArr[]=Arrays.copyOf(arr, arr.length+1);
+// 		newArr[arr.length]=element;
 		
 		System.out.println("Original Array: " + Arrays.toString(arr));
-		System.out.println("New Array: " +  Arrays.toString(newArr));
+		System.out.println("New Array: " +  list);
 		
 	}
 }

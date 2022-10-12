a6c51a3ae98a172162e1d0351500e88cd8b1308f
suksome <sakshamsai26sep@gmail.com>
2022-10-12 21:10:06 +0530

    Modified quick_sort.py

--- python/quick_sort.py
+++ python/quick_sort.py
@@ -1,37 +1,17 @@
+def sort(array):
+    less = []
+    equal = []
+    greater = []
 
-# Python program for implementation of Quicksort Sort
-
-def partition(array, low, high):
- 
-    pivot = array[high]
- 
-    i = low - 1
- 
-    for j in range(low, high):
-        if array[j] <= pivot:
-            i = i + 1
-            (array[i], array[j]) = (array[j], array[i])
-
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
-
-    return i + 1
-
-def quickSort(array, low, high):
-    if low < high:
- 
-        pi = partition(array, low, high)
- 
-        quickSort(array, low, pi - 1)
-        quickSort(array, pi + 1, high)
- 
- 
-arr = [112, 27, 14, -1, -10, 19, -2]
-print("Unsorted Array")
-print(arr)
- 
-size = len(arr)
- 
-quickSort(arr, 0, size - 1)
- 
-print('Sorted Array in Ascending Order:')
-print(arr)
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            elif x > pivot:
+                greater.append(x)
+        return sort(less)+equal+sort(greater) 
+    else: 
+        return array

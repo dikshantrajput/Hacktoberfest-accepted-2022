389c99ad2f7c9ade0f1c3a0af440d683364ef5b1
Chintan Jethi <chintanjethi711@gmail.com>
2022-10-13 14:57:55 +0530

    HACKTOBERFEST

---
+++ web-dev/Jquery Game/game.js
@@ -0,0 +1,73 @@
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+var level = 0;
+var started = false;
+var userClickedPattern = [];
+
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] == userClickedPattern[currentLevel]) {
+    console.log("success");
+    if (gamePattern.length == userClickedPattern.length) {
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(() => {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").html("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var r = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[r];
+  gamePattern.push(randomChosenColour);
+  console.log(gamePattern);
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function startOver() {
+  gamePattern = [];
+  level = 0;
+  started = false;
+}

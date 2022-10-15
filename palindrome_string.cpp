0cb3b123d6166d1a2dce6a4ffdb621b93d76d8f5
Naitik Sharma <73165529+Naitik234@users.noreply.github.com>
2022-10-16 04:27:22 +0530

    Create palindrome_string.cpp

---
+++ palindrome_string.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	    string str="";
+	    int m = S.length();
+	    for(int i=m-1;i>=0;--i){
+	        str = str+S[i];
+	    }
+	    if(S==str){
+	        return 1;
+	    }else{
+	       return 0;
+	    }
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends

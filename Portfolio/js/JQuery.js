962b1c912d3e3a60c19925205cccbb2522ab718e
Sneha2319 <snehamurugesh2019@gmail.com>
2022-10-23 22:45:12 +0530

    Added portfolio project

---
+++ Portfolio/js/JQuery.js
@@ -0,0 +1,37 @@
+const categoryTitle = document.querySelectorAll('.category-title');
+const allCategoryPosts = document.querySelectorAll('.all');
+
+for(let i = 0; i < categoryTitle.length; i++){
+    categoryTitle[i].addEventListener('click', filterPosts.bind(this, categoryTitle[i]));
+}
+
+function filterPosts(item){
+    changeActivePosition(item);
+    for(let i = 0; i < allCategoryPosts.length; i++){
+        if(allCategoryPosts[i].classList.contains(item.attributes.id.value)){
+            allCategoryPosts[i].style.display = "block";
+        } else {
+            allCategoryPosts[i].style.display = "none";
+        }
+    }
+}
+
+function changeActivePosition(activeItem){
+    for(let i = 0; i < categoryTitle.length; i++){
+        categoryTitle[i].classList.remove('active');
+    }
+    activeItem.classList.add('active');
+};
+
+
+var typed =new Typed(".auto-input",{
+    strings: ["Sneha"," a Web Developer", " a Data Analyst"],
+   typeSpeed:100,
+   backSpeed:100,
+   loop:true
+})
+
+
+function say(){
+   alert("submitted successfully")
+}

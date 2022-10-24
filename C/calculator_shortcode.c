82bdd27028ec000df9062d41a9d3cd671c2f36e1
Ankit Mukherjee <mukherjeeankit370@gmail.com>
2022-10-24 10:46:37 +0530

    MADE CHANGES IN THE CALCULATOR IT WILL NOW ALSO PERFORM POWER OF NUMBER

--- C/calculator_shortcode.c
+++ C/calculator_shortcode.c
@@ -4,6 +4,7 @@ int main() {
 
   char op;
   double first, second;
+  double a = 1;
   printf("Enter an operator (+, -, *, /): ");
   scanf("%c", &op);
   printf("Enter two operands: ");
@@ -21,6 +22,12 @@ int main() {
       break;
     case '/':
       printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+      break;
+    case'^':
+      for(int j =0; j<second; j++){
+        a = first * a;
+      }
+      printf("%.1lf ^ %.1lf = %.1lf",first, second, a);
       break;
     // operator doesn't match any case constant
     default:

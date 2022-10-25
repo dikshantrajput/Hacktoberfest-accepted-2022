dc1c67f9dc18907413eec0c98de7ec7d0d271a55
ananya321 <76964451+ananya321@users.noreply.github.com>
2022-10-25 11:15:47 +0530

    insertion_sort.py

---
+++ python/insertion_sort.py
@@ -0,0 +1,15 @@
+def insertion_sort(alist):
+    for i in range(1, len(alist)):
+        temp = alist[i]
+        j = i - 1
+        while (j >= 0 and temp < alist[j]):
+            alist[j + 1] = alist[j]
+            j = j - 1
+        alist[j + 1] = temp
+ 
+ 
+alist = input('Enter the list of numbers: ').split()
+alist = [int(x) for x in alist]
+insertion_sort(alist)
+print('Sorted list: ', end='')
+print(alist)

e7c7655b5d3034c4d52300c7be5eedf08cd8c50f
SARTHAK RANJAN <56467031+sarthaknerd@users.noreply.github.com>
2022-10-25 14:11:43 +0530

    Delete Element from Array

---
+++ cpp/deleteElements.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[10], tot=10, i, elem, j, found=0;
+    cout<<"Enter 10 Array Elements: ";
+    for(i=0; i<tot; i++)
+        cin>>arr[i];
+    cout<<"\nEnter Element to Delete: ";
+    cin>>elem;
+    for(i=0; i<tot; i++)
+    {
+        if(arr[i]==elem)
+        {
+            for(j=i; j<(tot-1); j++)
+                arr[j] = arr[j+1];
+            found++;
+            i--;
+            tot--;
+        }
+    }
+    if(found==0)
+        cout<<"\nElement doesn't found in the Array!";
+    else
+        cout<<"\nElement Deleted Successfully!";
+    cout<<endl;
+    return 0;
+}
